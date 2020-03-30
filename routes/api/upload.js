dfb755f9bc1eb8c802ab3c12321c245732779a5a
ZowWeb <wrongzoayb@gmail.com>
2020-03-31 04:06:54 +0530

    Improve watermark functionality 🔥 :fire: resize logo wrt image size logo and copyright text overlay wrt image size ✨ cleaner code and names

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -54,86 +54,100 @@ router.post("/", (req, res) => {
     // Watermark strategy
 
     // Logo to be put on the image
-    const imgLogo = "assets/images/watermark.png";
+    const imgLogo = "assets/images/logo-color.png";
 
     // work with backup image
     const imgActive = "assets/images/active/active.jpg";
 
     // Previews
-    const previewThumb = "assets/images/export/preview-thumb.jpg";
-    const preview = "assets/images/export/preview.jpg";
-    const previewBig = "assets/images/export/preview-big.jpg";
-    const textData = {
-      text: "© Zohaib Khan", //the text to be rendered on the image
-      maxWidth: 300, //image width - 10px margin left - 10px margin right
-      maxHeight: 200 + 20, //logo height + margin
-      placementX: 10, // 5px in on the x axis
-      placementY: 1024 - (72 + 20) - 10 //bottom of the image: height - maxHeight - margin
+    const previewThumb = `assets/images/export/preview-thumb__${req.file.filename}`;
+    const preview = `assets/images/export/preview__${req.file.filename}`;
+    const previewBig = `assets/images/export/preview-big__${req.file.filename}`;
+
+    const main = async () => {
+      const [image, logo] = await Promise.all([
+        Jimp.read(imgRaw),
+        Jimp.read(imgLogo)
+      ]);
+
+      const resizedLogo = logo.resize(image.bitmap.width / 5, Jimp.AUTO);
+
+      const X = (image.bitmap.width - logo.bitmap.width) / 2; //500-100
+      const Y = (image.bitmap.height - logo.bitmap.height) / 2;
+
+      // TEXT data
+      const textData = {
+        text: "© Zohaib Khan", //the text to be rendered on the image
+        maxWidth: image.bitmap.width, // max width wrt image
+        maxHeight: 0, // max height wrt image
+        placementX: 0, //  X axis
+        placementY: image.bitmap.height - 40 // Y axis wrt image
+      };
+
+      // READ template & clone raw image
+      Jimp.read(imgRaw)
+        .then(tpl => tpl.clone().write(imgActive))
+
+        // READ cloned (active) image
+        .then(() => Jimp.read(imgActive))
+
+        // COMPOSITE logo into image
+        .then(tpl =>
+          Jimp.read(resizedLogo).then(logoTpl => {
+            logoTpl.opacity(0.2);
+            
+            return tpl.composite(logoTpl, X, Y, {
+              mode: Jimp.BLEND_SOURCE_OVER,
+              opacityDest: 0.5,
+              opacitySource: 0.9
+            });
+          })
+        )
+
+        // LOAD font
+        .then(tpl =>
+          Jimp.loadFont(Jimp.FONT_SANS_32_WHITE).then(font => [tpl, font])
+        )
+
+        // WATERMARK text
+        .then(data => {
+          tpl = data[0];
+          font = data[1];
+
+          return tpl.print(
+            font,
+            textData.placementX,
+            textData.placementY,
+            {
+              text: textData.text,
+              alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER
+            },
+            textData.maxWidth
+          );
+        })
+
+        //EXPORT images
+        .then(tpl => {
+          // Preview Big
+          tpl
+            .resize(Jimp.AUTO, 850)
+            .quality(90)
+            .write(previewBig);
+          // Preview
+          tpl
+            .resize(Jimp.AUTO, 500)
+            .quality(80)
+            .write(preview);
+          // Preview Thumbnail
+          tpl
+            .resize(Jimp.AUTO, 350)
+            .quality(70)
+            .write(previewThumb);
+        });
     };
 
-    // READ template & clone raw image
-    Jimp.read(imgRaw)
-      .then(tpl => tpl.clone().write(imgActive))
-
-      // READ cloned (active) image
-      .then(() => Jimp.read(imgActive))
-
-      // COMPOSITE logo into image
-      .then(tpl =>
-        Jimp.read(imgLogo).then(logoTpl => {
-          logoTpl.opacity(0.2);
-          return tpl.composite(logoTpl, 512 - 75, 350 - 40, [
-            Jimp.BLEND_DESTINATION_OVER,
-            0.2,
-            0.2
-          ]);
-        })
-      )
-
-      // LOAD font
-      .then(tpl =>
-        Jimp.loadFont(Jimp.FONT_SANS_32_WHITE).then(font => [tpl, font])
-      )
-
-      // WATERMARK text
-      .then(data => {
-        tpl = data[0];
-        font = data[1];
-
-        return tpl.print(
-          font,
-          textData.placementX,
-          textData.placementY,
-          {
-            text: textData.text,
-            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-            alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
-          },
-          textData.maxWidth,
-          textData.maxHeight
-        );
-      })
-
-      //EXPORT images
-      .then(tpl => {
-        // Preview Big
-        tpl
-          .resize(Jimp.AUTO, 850)
-          .quality(90)
-          .write(previewBig);
-        // Preview
-        tpl
-          .resize(Jimp.AUTO, 500)
-          .quality(80)
-          .write(preview);
-        // Preview Thumbnail
-        tpl
-          .resize(Jimp.AUTO, 350)
-          .quality(70)
-          .write(previewThumb);
-      })
-
-      // SEND JSON as response
+    // SEND JSON as response
+    main()
       .then(tpl =>
         res.json({
           status: true,
